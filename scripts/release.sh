8628d4be664e981d508d51d320bdd79377ddf391
Daniel Kuehn <daniel@kuehn.foo>
2024-06-15 20:35:34 -0400

    scripts(release): refactor for new repo structure

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,9 +2,21 @@
 
 set -e
 
+go test -v wordlists_test.go
+
+PKGS=("eff" "names")
+
+select pkg_choice in "${PKGS[@]}"; do
+	case "$pkg_choice" in
+		"eff") PKG="eff"; break ;;
+		"names") PKG="names"; break ;;
+		*) echo "Invalid option."; contine ;;
+	esac
+done
+
+cd "$PKG"
+
 VERSION="$(<VERSION)"
 
-git tag -s "$VERSION" -m "Release $VERSION"
-git push origin "$VERSION"
-
-gh release new "$VERSION" --verify-tag
+git tag -s "$PKG/$VERSION" -m "Release $VERSION"
+git push origin "$PKG/$VERSION"
