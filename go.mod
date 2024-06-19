fd6e0e449c89e2109bb0c9741bd2518f263c81c4
Daniel Kuehn <daniel@kuehn.foo>
2024-06-19 11:44:01 -0400

    feat: add root module

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/wordgen/wordlists
+
+go 1.22.4
+
+require (
+	github.com/wordgen/wordlists/eff v0.2.0
+	github.com/wordgen/wordlists/names v0.2.0
+)
---
+++ wordlists.go
@@ -0,0 +1,16 @@
+package wordlists
+
+import (
+	"github.com/wordgen/wordlists/eff"
+	"github.com/wordgen/wordlists/names"
+)
+
+var (
+	EffLarge  = eff.Large
+	EffShort1 = eff.Short1
+	EffShort2 = eff.Short2
+
+	NamesFemale = names.Female
+	NamesMale   = names.Male
+	NamesMixed  = names.Mixed
+)
